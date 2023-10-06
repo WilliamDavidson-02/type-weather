a6a9d25bb5ae6c14e44ce617934c8eff37c0b348
William Davidson <wj.davidson@outlook.com>
2023-10-06 13:40:08 +0200

    city search suggestions with geoapify

--- src/components/shared/SearchContainer.jsx
+++ src/components/shared/SearchContainer.jsx
@@ -1,22 +1,54 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function SearchContainer() {
   const [weatherSearch, setWeatherSearch] = useState("");
+  const [searchSuggestions, setSearchSuggestions] = useState([]);
+  const geoUrl = `https://api.geoapify.com/v1/geocode/autocomplete?format=json&type=city&lang=en&text=${weatherSearch}&apiKey=${
+    import.meta.env.VITE_GEO_KEY
+  }`;
 
   function handleWeatherSubmit(ev) {
     ev.preventDefault();
     console.log({ weatherSearch });
   }
 
+  function handleSearchChange(ev) {
+    setWeatherSearch(ev.target.value);
+    if (weatherSearch.length >= 2) {
+      axios
+        .get(geoUrl)
+        .then((response) => setSearchSuggestions([...response.data.results]))
+        .catch((err) => console.log(err));
+    }
+  }
+
   return (
-    <form className="w-full flex justify-center" onSubmit={handleWeatherSubmit}>
+    <form
+      className="relative w-full flex justify-center"
+      onSubmit={handleWeatherSubmit}
+    >
       <input
-        className="bg-gray-600 text-gray-400 px-5 py-3 rounded-lg outline-none w-full max-w-[500px]"
+        className=" bg-gray-600 text-gray-400 px-5 py-4 rounded-lg outline-none w-full max-w-[500px]"
         value={weatherSearch}
-        onChange={(ev) => setWeatherSearch(ev.target.value)}
+        onChange={handleSearchChange}
         type="text"
         placeholder="Search location"
       />
+      {searchSuggestions.length > 0 && (
+        <div className="absolute w-full max-w-[500px] top-16 bg-gray-500 rounded-lg shadow-lg flex flex-col">
+          {searchSuggestions.map((suggestion) => (
+            <button
+              type="submit"
+              onClick={() => setWeatherSearch(suggestion.address_line1)}
+              className="cursor-pointer hover:bg-gray-400 transition duration-300 first:rounded-t-lg last:rounded-b-lg px-5 py-4 border border-transparent border-b-gray-900 text-gray-100 text-start outline-none"
+              key={suggestion.place_id}
+            >
+              {suggestion.address_line1}
+            </button>
+          ))}
+        </div>
+      )}
     </form>
   );
 }
